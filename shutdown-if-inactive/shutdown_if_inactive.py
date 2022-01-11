a028d16629b105230c6dc537a6bb8127654b15c5
Alan Borsato <37001712+aborsato@users.noreply.github.com>
2022-01-11 17:13:25 -0600

    Adding terminals to check inactivity.

--- shutdown-if-inactive/shutdown_if_inactive.py
+++ shutdown-if-inactive/shutdown_if_inactive.py
@@ -5,10 +5,11 @@ from azureml.core import Workspace
 from azureml.core.compute import ComputeInstance
 from datetime import datetime
 
-idle_threshold_in_sec = 3600
+IDLE_THRESHOLD_IN_SEC = 3600
 
 # Jupyter runs on Compute Instance on http on port 8888
-notebook_session_url = f'http://localhost:8888/api/sessions'
+NOTEBOOK_SESSION_URL = f'http://localhost:8888/api/sessions'
+NOTEBOOK_TERMINAL_URL = f'http://localhost:8888/api/terminals'
 
 def get_compute_instance_name():
     instance = None
@@ -18,36 +19,37 @@ def get_compute_instance_name():
             instance = dict(x.strip().split('=') for x in f)
     return instance['instance']
 
-def get_notebook_sessions():
-    response = requests.get(notebook_session_url)
-    response = response.json()  
-    notebooks = []
-    for nb in response:
-        path = nb['path']
-        state = nb['kernel']['execution_state']
-        num_connections = nb['kernel']['connections']
-        last_activity = nb['kernel']['last_activity']
-        notebooks.append({
-            "path": path,
-            "state": state,
-            "num_connections": num_connections,
-            "last_activity": last_activity
-        })
-    return notebooks
+def was_recently_updated(ts):
+    timestamp = datetime.strptime(ts, "%Y-%m-%dT%H:%M:%S.%fz")
+    return (datetime.now() - timestamp).total_seconds() < IDLE_THRESHOLD_IN_SEC
 
-def was_notebook_recently_updated(notebook):
-    last_activity = datetime.strptime(notebook['last_activity'],"%Y-%m-%dT%H:%M:%S.%fz")
-    return (datetime.now() - last_activity).total_seconds() < idle_threshold_in_sec
+def get_notebook_sessions():
+    notebooks = requests.get(NOTEBOOK_SESSION_URL).json()
+    return [{
+        "path": nb['path'],
+        "state": nb['kernel']['execution_state'],
+        "num_connections": nb['kernel']['connections'],
+        "last_activity": nb['kernel']['last_activity'],
+        "recently_updated": was_recently_updated(nb['kernel']['last_activity']),
+    } for nb in notebooks]
+
+def get_notebook_terminals():
+    terminals = requests.get(NOTEBOOK_TERMINAL_URL).json()
+    return [{
+        "name": tr["name"],
+        "last_activity": tr['last_activity'],
+        "recently_updated": was_recently_updated(tr['last_activity']),
+    } for tr in terminals]
 
 def are_all_notebooks_idle(notebooks):
     for n in notebooks:
-        print('Notebook:',  n['path'])
-        print('Notebook state:',  n['state'])
-        print('Notebook connections:',  n['num_connections'])
-        print('Was notebook recently updated:', was_notebook_recently_updated(n))
-        if (n['state'] != 'idle' or
-            n['num_connections'] > 0 or
-            was_notebook_recently_updated(n)):
+        if (n['state'] != 'idle' or n["recently_updated"]):
+            return False
+    return True
+
+def are_all_terminals_idle(terminals):
+    for t in terminals:
+        if (t["recently_updated"]):
             return False
     return True
 
@@ -56,16 +58,30 @@ def get_instance_uptime():
         uptime_seconds = float(f.readline().split()[0])
     return uptime_seconds
 
-notebooks = get_notebook_sessions()
-ci_name = get_compute_instance_name()
+def is_compute_idle(uptime):
+    return uptime > IDLE_THRESHOLD_IN_SEC
 
-if are_all_notebooks_idle(notebooks) and get_instance_uptime() > idle_threshold_in_sec:
-    print(f'Compute Instance {ci_name} will be shut down!')
+notebooks = get_notebook_sessions()
+terminals = get_notebook_terminals()
+ci_name = get_compute_instance_name()
+instance_uptime = get_instance_uptime()
+
+# print debug data to console
+print(f"Checking if Compute Instance {ci_name} is idle")
+print(f"> MAX Idle Time: {IDLE_THRESHOLD_IN_SEC} s")
+print(f"> VM Uptime:     {instance_uptime} s")
+print()
+print("> Notebooks")
+print(json.dumps(notebooks, indent=2))
+print("> Terminals")
+print(json.dumps(terminals, indent=2))
+print("")
+
+if are_all_notebooks_idle(notebooks) and are_all_terminals_idle(terminals) and instance_uptime > IDLE_THRESHOLD_IN_SEC:
+    print('RESULT: Compute Instance is idle and will be shut down.')
     # Connect to workspace and stop instance
     ws = Workspace.from_config()
     ct = ComputeInstance(ws, ci_name)
     ct.stop(wait_for_completion=False, show_output=False)
 else:
-    print(f'Compute Instance {ci_name} still has notebooks running or just started, will not shut it down')
-    print("Details:")
-    print(json.dumps(notebooks, indent=2))
+    print('RESULT: Compute Instance has notebooks and terminals running or just started, will NOT be shut down.')
